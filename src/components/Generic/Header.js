2a1833f71cd089acdb94ea4007c3063484d828f5
Nick Alexander Caris <nickcaris97@gmail.com>
2020-04-12 13:29:24 -0400

    added css to site

--- src/components/Generic/Header.js
+++ src/components/Generic/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Header = () => (
     <div>
-        <img src="../../../public/logo.png" alt="DQ logo" height="42" width="42"/>
+        <img src="../../logo.png" alt="DQ logo" height="42" width="42"/>
         <p>This is a header</p>
     </div>
 );
