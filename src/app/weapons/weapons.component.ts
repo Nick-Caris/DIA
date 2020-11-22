df379fd25cde5406b16da10c77fa44a60bf3ae19
Nick Alexander Caris <nickcaris97@gmail.com>
2020-11-22 15:33:54 +0100

    temp list for weapons

--- src/app/weapons/weapons.component.ts
+++ src/app/weapons/weapons.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeaponsComponent implements OnInit {
 
+  weapons = [
+    // tslint:disable-next-line:max-line-length
+    {id: 1, weapon: 'SABRE', grip: 'Links', handedness: 'Links', is_working: true, comment: 'hij werkt wel maar doet raar op woensdagen', is_borrowed: false, electric: true},
+    {id: 2, weapon: 'FOIL', grip: 'Rechts', handedness: 'Links', is_working: false, comment: null, is_borrowed: false, electric: false}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
